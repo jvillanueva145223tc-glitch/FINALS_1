e124ddd81ff19f1533551973f9a427f31968bb71
Renneyx Remedios <r.remedios.144575.tc@umindanao.edu.ph>
2025-12-17 01:35:15 +0800

    Added Interface, Models, MySQLDBContext for class managing. Added User base class, Admin class, and Facilty class.

---
+++ Models/Admin.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.Models
+{
+    // admin class
+    public class Admin : User
+    {
+        public Admin()
+        {
+            Role = "Admin";
+        }
+        public override string GetDashboardMessage()
+        {
+            return "Welcome Admin! You have full system access.";
+        }
+        public override string GetUserInfo()
+        {
+            return $"Administrator: {FullName}";
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.Models
+{
+    //user base class
+    public abstract class User
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public abstract string GetDashboardMessage();
+
+        public virtual string GetUserInfo()
+        {
+            return $"{FullName} {Role}";
+        }
+
+    }
+}
