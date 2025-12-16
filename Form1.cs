5c4af13505cb24c5b6e0470292aeebf8efd9049e
jvillanueva145223tc-glitch <j.villanueva.145223.tc@umindanao.edu.ph>
2025-12-17 03:37:53 +0800

    Added Text,label,button,combobox(log in ui)

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,30 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
