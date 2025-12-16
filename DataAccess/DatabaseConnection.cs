bb32a1c8c5f45107c7a30179ed82e16ebffe31bc
Renneyx Remedios <r.remedios.144575.tc@umindanao.edu.ph>
2025-12-17 02:56:07 +0800

    added ScheduleConflict checker class and instaled MySQL.data and added DatabaseConnection class

---
+++ DataAccess/DatabaseConnection.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.DataAccess
+{
+    /// <summary>
+    /// Singleton class for managing database connections
+    /// </summary>
+    public sealed class DatabaseConnection
+    {
+        private static DatabaseConnection _instance = null;
+        private static readonly object _lock = new object();
+        private readonly string _connectionString;
+
+        // Private constructor to prevent instantiation
+        private DatabaseConnection()
+        {
+            // Update with your MySQL credentials
+            _connectionString = "Server=localhost;Port=3306;Database=subject_loading_db;User ID=root;Password=applePIE21 passw0ord;";
+        }
+
+        /// <summary>
+        /// Gets the singleton instance of DatabaseConnection
+        /// </summary>
+        public static DatabaseConnection Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (_lock)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new DatabaseConnection();
+                        }
+                    }
+                }
+                return _instance;
+            }
+        }
+
+        /// <summary>
+        /// Gets a new MySQL connection
+        /// </summary>
+        /// <returns>MySqlConnection object</returns>
+        public MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(_connectionString);
+        }
+
+        /// <summary>
+        /// Tests the database connection
+        /// </summary>
+        /// <returns>True if connection successful, false otherwise</returns>
+        public bool TestConnection()
+        {
+            try
+            {
+                using (var connection = GetConnection())
+                {
+                    connection.Open();
+                    return true;
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
+
+
+        
